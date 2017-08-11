f917348fb35e22471cac4ae96280aef5617b5225
jommersu <jommer.su@foxmail.com>
2017-08-11 23:00:12 +0800

    task7

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -6,26 +6,38 @@ var sinonChai = require("sinon-chai");
 var expect = chai.expect;
 chai.use(sinonChai);
 
-var main = require("../lib/main.js");
+let main = require("../lib/main.js");
+let Lyrics = require("../lib/Lyrics.js");
+let Lyrics_99 = Lyrics.Lyrics_99;
+let Lyrics_2 = Lyrics.Lyrics_2;
+let Lyrics_1 = Lyrics.Lyrics_1;
+let Lyrics_0 = Lyrics.Lyrics_0;
 
+describe("printLyrics", function(){
+    //sinon.spy(console, 'log');
 
-describe("测试描述", function(){
-    sinon.spy(console, 'log');
-
-    it("测试用例1", function(){
-
-        var result = main();
-        var expect_string = '';
-        
-        expect(expect_string).to.equal(result);
+    it("when number is 99", function(){
+        let number = 99;
+        let result = main(number);
+        let expect_string = Lyrics_99();
+        expect(result).to.equal(expect_string);
     });
-
-    it("测试用例2", function(){
-
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
-
-        expect(expect_string).to.equal(result);
+    it("when number is 2", function(){
+        let number = 2;
+        let result = main(number);
+        let expect_string = Lyrics_2();
+        expect(result).to.equal(expect_string);
+    });
+    it("when number is 1", function(){
+        let number = 1;
+        let result = main(number);
+        let expect_string = Lyrics_1();
+        expect(result).to.equal(expect_string);
+    });
+    it("when number is 0", function(){
+        let number = 0;
+        let result = main(number);
+        let expect_string = Lyrics_0();
+        expect(result).to.equal(expect_string);
     });
 });
